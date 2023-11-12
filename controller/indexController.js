f360724b0fb64598d4ea99a74fdbe62bfcdb5648
pd0104 <philjen35@yahoo.com>
2023-11-12 13:48:47 -0800

    Development changes

--- controller/indexController.js
+++ controller/indexController.js
@@ -1,7 +1,26 @@
 const bcrypt = require("bcryptjs");
+const fs = require('fs');
+const readline = require('readline');
 
-exports.get_index = (req, res) => {
+exports.get_index =  (req, res) => {
     const error = req.session.error;
     delete req.session.error;
-    res.render("index", {title: 'Home',err: error});
+
+
+    fs.readFile('./image/productImageLocation.txt', 
+        function(err, data) {
+            if(err) throw err; 
+            var array = data.toString().split("/abc");
+            res.render("index", {title: 'Home',err: error,data:array});
+        }
+    );
+
+     
+
+    
 };
+
+const readFileLines = filename =>
+   fs.readFileSync(filename)
+   .toString('UTF8')
+   .split('/n');
